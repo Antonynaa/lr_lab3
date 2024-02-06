dd7f1f43dde87040bcf497016af537939e15419e
Cojocari Antonina <cojocari.antonina@elev.cihcahul.md>
2024-02-06 11:11:51 +0200

    Relatii, migrare, rute, model Phone

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PhoneController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -8,3 +9,13 @@ Route::redirect('/', '/users');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store']) ->name('users.store');
+
+Route::controller(PhoneController::class)->group(function(){
+    Route::get('phones', 'index');
+    Route::get('phones/create', 'create');
+    Route::post('phones', 'store');
+    Route::get('phones/{phone}/edit', 'edit');
+    Route::get('phones/{phone}', 'show');
+    Route::put('phones/{phone}/update', 'update');
+    Route::delete('phones/{phone}', 'destroy');
+});
